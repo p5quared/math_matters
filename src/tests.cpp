ccd50d1bfe28e28235b3f8bf29d6f8601e2f9b4b
Peter V <98245483+p5quared@users.noreply.github.com>
2023-04-01 23:05:25 -0400

    add many tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -13,22 +13,32 @@ TEST_CASE("Pre-Flight")
 }
 
 
-TEST_CASE("Valid Characters", "[validCharacters] [sanitization]")
+TEST_CASE("Valid Characters", "[checkValidCharacters] [sanitization]")
 {
     using namespace psv;
     // equations with one or more invalid characters
     equation invalidChars = "1 + 1 - 3 * 4 / 5 ^ 6 @";
     equation invalidChars2 = "1 + 1 - 3 * 4 / 5 ^ 6 #";
-    equation invalidChars3 = "1 + 1 - 3 * 4 / 5 ^ 6 $";
+    equation invalidChars3 = "1 + 1% - 3 * 4 / 5 ^ 6 $";
 
     // valid equation
     equation validEq = "1 + 1 - 3 * 4 / 5 ^ 6";
 
-    REQUIRE(validCharacters(invalidChars) == false);
-    REQUIRE(validCharacters(invalidChars2) == false);
-    REQUIRE(validCharacters(invalidChars3) == false);
+    REQUIRE(checkValidCharacters(invalidChars) == false);
+    REQUIRE(invalid_characters.size() == 1);
+    REQUIRE(*invalid_characters[0] == '@');
 
-    REQUIRE(validCharacters(validEq) == true);
+    REQUIRE(checkValidCharacters(invalidChars2) == false);
+    REQUIRE(invalid_characters.size() == 1);
+    REQUIRE(*invalid_characters[0] == '#');
+
+    REQUIRE(checkValidCharacters(invalidChars3) == false);
+    REQUIRE(invalid_characters.size() == 2);
+    REQUIRE(*invalid_characters[0] == '%');
+    REQUIRE(*invalid_characters[1] == '$');
+
+    REQUIRE(checkValidCharacters(validEq) == true);
+    REQUIRE(invalid_characters.empty());
 }
 
 TEST_CASE("Eliminate Whitespace", "[eliminateWhitespace] [sanitization]")
@@ -72,51 +82,26 @@ TEST_CASE("Scoping", "[scoping] [sanitization]")
 
 
     SECTION("Valid Scoping") {
-        REQUIRE(validScoping(validEq) == true);
-        REQUIRE(validScoping(validEq2) == true);
-        REQUIRE(validScoping(validEq3) == true);
-        REQUIRE(validScoping(validEq4) == true);
-        REQUIRE(validScoping(validEq5) == true);
-        REQUIRE(validScoping(validEq6) == true);
+        REQUIRE_NOTHROW(validScoping(validEq));
+        REQUIRE_NOTHROW(validScoping(validEq2));
+        REQUIRE_NOTHROW(validScoping(validEq3));
+        REQUIRE_NOTHROW(validScoping(validEq4));
+        REQUIRE_NOTHROW(validScoping(validEq5));
+        REQUIRE_NOTHROW(validScoping(validEq6));
     }
 
     SECTION("Invalid Scoping") {
-        REQUIRE(validScoping(invalidEq) == false);
-        REQUIRE(validScoping(invalidEq2) == false);
-        REQUIRE(validScoping(invalidEq3) == false);
-        REQUIRE(validScoping(invalidEq4) == false);
-        REQUIRE(validScoping(invalidEq5) == false);
-        REQUIRE(validScoping(invalidEq6) == false);
-    }
-}
+        REQUIRE_THROWS(validScoping(invalidEq), "Invalid Scoping");
 
-TEST_CASE("Operator Usage", "[operatorUsage] [sanitization]")
-{
-    using namespace psv;
-    SECTION("Valid Operator Usage") {
-        equation validEq = "1+1-3*4/5^6";
-        equation validEq2 = "1+(3)";
-        equation validEq3 = "1+(3+1)";
+        REQUIRE_THROWS(validScoping(invalidEq2), "Invalid Scoping");
 
-        REQUIRE(all_validOperatorUsage(validEq) == true);
-        REQUIRE(all_validOperatorUsage(validEq2) == true);
-        REQUIRE(all_validOperatorUsage(validEq3) == true);
-    }
+        REQUIRE_THROWS(validScoping(invalidEq3), "Invalid Scoping");
 
-    SECTION("Invalid Operator Usage"){
-        equation invalidEq4 = "1+1-3*4/5^6+";
-        equation invalidEq5 = "+1+1-3*4/5^6";
-        equation invalidEq6 = "1+*1-3*4/5^6";
+        REQUIRE_THROWS(validScoping(invalidEq4), "Invalid Scoping");
 
-        // invalid parentheses
-        equation invalidEq = "1+1-3*4/5^6*)";
-        equation invalidEq2 = "1+1-3*)4/5^6*(";
+        REQUIRE_THROWS(validScoping(invalidEq5), "Invalid Scoping");
 
-        REQUIRE(all_validOperatorUsage(invalidEq4) == false);
-        REQUIRE(all_validOperatorUsage(invalidEq5) == false);
-        REQUIRE(all_validOperatorUsage(invalidEq6) == false);
-        REQUIRE(all_validOperatorUsage(invalidEq) == false);
-        REQUIRE(all_validOperatorUsage(invalidEq2) == false);
+        REQUIRE_THROWS(validScoping(invalidEq6), "Invalid Scoping");
     }
 }
 
@@ -144,158 +129,196 @@ TEST_CASE("isOperator", "[operatorUsage] [sanitization]")
     REQUIRE(isOperator('*') == true);
     REQUIRE(isOperator('/') == true);
     REQUIRE(isOperator('^') == true);
-    REQUIRE(isOperator('(') == false);
-    REQUIRE(isOperator(')') == false);
+    REQUIRE(isOperator('n') == true);
+    REQUIRE(isOperator('(') == true);
+    REQUIRE(isOperator(')') == true);
     REQUIRE(isOperator('1') == false);
     REQUIRE(isOperator('a') == false);
     REQUIRE(isOperator(' ') == false);
 }
 
-TEST_CASE("Negative Numbers", "[operatorUsage] [sanitization]")
-{
-    using namespace psv;
-    // negative numbers should be allowed
-    // we may consider that a - may precede a number a (, or another -
-    equation eq = "1+1-3*-4/5^6";
-    equation eq2 = "1+1-3*-4/5^-6";
-    equation eq3 = "-1+1-3*-4/5";
-
-
-    REQUIRE(all_validOperatorUsage(eq) == true);
-    REQUIRE(all_validOperatorUsage(eq2) == true);
-    REQUIRE(all_validOperatorUsage(eq3) == true);
-}
-
-
-TEST_CASE("Sanitize Equation", "[sanitization] [integration]"){
-    // sanitizeEquation should return True if the equation is valid, and False if it is not
-    // it should also modify the equation to remove whitespace
-    using namespace psv;
-
-    // valid expression
-    equation eq = "1 + 1 - 3 * 4 / 5 ^ 6";
-
-    // valid expressions with parentheses usage
-    equation eq2 = "(1 + 1 - 3 * 4 / 5 ^ 6)";
-    equation eq3 = "((1 + 1 - 3 * 4 / 5 ^ 6))";
-    equation eq4 = "((1 + 1 - 3 * 4 / 5 ^ 6) + 1)";
-    equation eq5 = "((1 + 1 - 3 * 4 / 5 ^ 6) + 1) - 1";
-
-    // improper parentheses pairs
-    equation eq6 = "1 + 1 - 3 * 4 / 5 ^ 6) + 1";
-    equation eq7 = "1 + 1 - 3 * 4 / 5 ^ 6 + 1)";
-    equation eq8 = "(1 + 1 - 3 * 4 / 5 ^ 6 + 1";
-    equation eq9 = "1 + 1 - 3 * 4 / 5 ^ 6 + 1(";
-
-    // improper operator usage
-    equation eq10 = "1 + 1 - 3 * 4 / 5 ^ 6 +";
-    equation eq11 = "*1 + 1 - 3 * 4 / 5 ^ 6 + 1";
-
-    // improper operator locations
-    equation eq12 = "1 + 1 - 3 * 4 / 5 ^ 6 +";
-    equation eq13 = "1 + 1 - 3 * 4 / 5 ^ 6 *+ 1";
-
-    REQUIRE(sanitizeEquation(eq) == true);
-    REQUIRE(eq == "1+1-3*4/5^6");
-
-    REQUIRE(sanitizeEquation(eq2) == true);
-    REQUIRE(eq2 == "(1+1-3*4/5^6)");
-
-    REQUIRE(sanitizeEquation(eq3) == true);
-    REQUIRE(eq3 == "((1+1-3*4/5^6))");
-
-    REQUIRE(sanitizeEquation(eq4) == true);
-    REQUIRE(eq4 == "((1+1-3*4/5^6)+1)");
-
-    REQUIRE(sanitizeEquation(eq5) == true);
-    REQUIRE(eq5 == "((1+1-3*4/5^6)+1)-1");
-
-    REQUIRE(sanitizeEquation(eq6) == false);
-    REQUIRE(eq6 == "1+1-3*4/5^6)+1");
-
-    REQUIRE(sanitizeEquation(eq7) == false);
-    REQUIRE(eq7 == "1+1-3*4/5^6+1)");
-
-    REQUIRE(sanitizeEquation(eq8) == false);
-    REQUIRE(eq8 == "(1+1-3*4/5^6+1");
-
-    REQUIRE(sanitizeEquation(eq9) == false);
-    REQUIRE(eq9 == "1+1-3*4/5^6+1(");
-
-    REQUIRE(sanitizeEquation(eq10) == false);
-    REQUIRE(eq10 == "1+1-3*4/5^6+");
-
-    REQUIRE(sanitizeEquation(eq11) == false);
-    REQUIRE(eq11 == "*1+1-3*4/5^6+1");
-
-    REQUIRE(sanitizeEquation(eq12) == false);
-    REQUIRE(eq12 == "1+1-3*4/5^6+");
-
-    REQUIRE(sanitizeEquation(eq13) == false);
-    REQUIRE(eq13 == "1+1-3*4/5^6*+1");
-}
-
 TEST_CASE("Stack Class"){
 using namespace psv;
-    // stack class should be able to push and pop values
+    // stack class should be able to place and pop values
     // it should also be able to return the top value
-    Stack<int> s;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.push(4);
-    s.push(5);
+    SECTION("Standard Functions") {
+        Stack<int> s;
+        s.place(1);
+        s.place(2);
+        s.place(3);
+        s.place(4);
+        s.place(5);
 
-    REQUIRE(s.top() == 5);
-    s.pop();
-    REQUIRE(s.top() == 4);
-    s.pop();
-    REQUIRE(s.top() == 3);
-    s.pop();
-    REQUIRE(s.top() == 2);
-    s.pop();
-    REQUIRE(s.top() == 1);
-    s.pop();
-    REQUIRE(s.isEmpty() == true);
+        REQUIRE(s.top() == 5);
+        s.pop();
+        REQUIRE(s.top() == 4);
+        s.pop();
+        REQUIRE(s.top() == 3);
+        s.pop();
+        REQUIRE(s.top() == 2);
+        s.pop();
+        REQUIRE(s.top() == 1);
+        s.pop();
+        REQUIRE(s.isEmpty() == true);
+    }
 
+    struct Car {
+        int year;
+        std::string make;
+        std::string model;
+        Car(int y, std::string m, std::string mo) : year(y), make(m), model(mo) {}
+        Car() : year(0), make(""), model("") {}
+
+    };
+    SECTION("Custom Objects"){
+        Car car1 = Car(2019, "Ford", "Mustang");
+        Car car2 = Car(2018, "Ford", "F-150");
+        Car car3 = Car(2017, "Ford", "Fusion");
+        Car car4 = Car(2016, "Ford", "Focus");
+        Car car5 = Car(2015, "Ford", "Explorer");
+
+        Stack<Car> car_stack;
+        car_stack.place(Car(2019, "Ford", "Mustang"));
+        REQUIRE(car_stack.top().year == 2019);
+        REQUIRE(car_stack.top().make == "Ford");
+
+        car_stack.place(car2);
+        REQUIRE(car_stack.top().year == 2018);
+        REQUIRE(car_stack.top().make == "Ford");
+
+        car_stack.pop();
+        REQUIRE(car_stack.top().year == 2019);
+
+        car_stack.place(car3);
+        car_stack.place(car4);
+        car_stack.place(car5);
+        REQUIRE(car_stack.top().year == 2015);
+        REQUIRE(car_stack.size() == 4);
+
+    }
+
+    // Emplace is way cooler than place... |-D-D| (cool-sunglasses)
     SECTION("Emplace"){
-        // stack should be able to emplace custom objects
-        struct car {
-            int year;
-            std::string make;
-            std::string model;
-            car(int y=0, std::string m="", std::string mo="") : year(std::move(y)), make(std::move(m)), model
-            (std::move(mo)) {}
+        Stack<Car> car_stack;
+        car_stack.emplace(2019, "Ford", "Mustang");
+        car_stack.emplace(2018, "Ford", "F-150");
+        car_stack.emplace(2017, "Ford", "Fusion");
+        car_stack.emplace(2016, "Ford", "Focus");
+        car_stack.emplace(2015, "Ford", "Explorer");
 
-            ~car() = default;
-        };
-        Stack<car> s2;
-        s2.emplace(2019, "Ford", "Mustang");
-        s2.emplace(2018, "Ford", "F-150");
-        s2.emplace(2017, "Ford", "Fusion");
-        s2.emplace(2016, "Ford", "Focus");
-        s2.emplace(2015, "Ford", "Explorer");
-
-        REQUIRE(s2.top().year == 2015);
-        REQUIRE(s2.top().make == "Ford");
-        REQUIRE(s2.top().model == "Explorer");
-        s2.pop();
-        REQUIRE(s2.top().year == 2016);
-        REQUIRE(s2.top().make == "Ford");
-        REQUIRE(s2.top().model == "Focus");
-        s2.pop();
-        REQUIRE(s2.top().year == 2017);
-        REQUIRE(s2.top().make == "Ford");
-        REQUIRE(s2.top().model == "Fusion");
-        s2.pop();
-        REQUIRE(s2.top().year == 2018);
-        REQUIRE(s2.top().make == "Ford");
-        REQUIRE(s2.top().model == "F-150");
-        s2.pop();
-        REQUIRE(s2.top().year == 2019);
-        REQUIRE(s2.top().make == "Ford");
-        REQUIRE(s2.top().model == "Mustang");
-        s2.pop();
-        REQUIRE(s2.isEmpty() == true);
+        REQUIRE(car_stack.top().year == 2015);
+        REQUIRE(car_stack.top().make == "Ford");
+        REQUIRE(car_stack.top().model == "Explorer");
+        car_stack.pop();
+        REQUIRE(car_stack.top().year == 2016);
+        REQUIRE(car_stack.top().make == "Ford");
+        REQUIRE(car_stack.top().model == "Focus");
+        car_stack.pop();
+        REQUIRE(car_stack.top().year == 2017);
+        REQUIRE(car_stack.top().make == "Ford");
+        REQUIRE(car_stack.top().model == "Fusion");
+        car_stack.pop();
+        REQUIRE(car_stack.top().year == 2018);
+        REQUIRE(car_stack.top().make == "Ford");
+        REQUIRE(car_stack.top().model == "F-150");
+        car_stack.pop();
+        REQUIRE(car_stack.top().year == 2019);
+        REQUIRE(car_stack.top().make == "Ford");
+        REQUIRE(car_stack.top().model == "Mustang");
+        car_stack.pop();
+        REQUIRE(car_stack.isEmpty() == true);
     }
 }
+
+std::vector<char> as_vector(std::string s){
+    std::vector<char> v;
+    for(auto c : s){
+        v.push_back(c);
+    }
+    return v;
+}
+TEST_CASE("Parse Equation"){
+    using namespace psv;
+    SECTION("Valid Statements"){
+        // Standard statement in order
+       std::string valid1 = "1+1-3*4/5^6";
+       std::vector<char> valid1_parsed;
+       REQUIRE_NOTHROW(valid1_parsed = parseStatement(valid1));
+       REQUIRE(valid1_parsed == as_vector("11+34*56^/-"));
+
+       // Statement with operators out of order
+       std::string valid2 = "1+1-3*4/5^6+1";
+       std::vector<char> valid2_parsed;
+       REQUIRE_NOTHROW(valid2_parsed = parseStatement(valid2));
+       REQUIRE(valid2_parsed == as_vector("11+34*56^/-1+"));
+
+       // Statement with parenthesis affecting order of operations
+       std::string valid3 = "(1+1)*3";
+       std::vector<char> valid3_parsed;
+       REQUIRE_NOTHROW(valid3_parsed = parseStatement(valid3));
+       REQUIRE(valid3_parsed == as_vector("11+3*"));
+
+
+       // Statement with two parenthesis
+         std::string valid4 = "(1+1)*(3+3)";
+            std::vector<char> valid4_parsed;
+            REQUIRE_NOTHROW(valid4_parsed = parseStatement(valid4));
+            REQUIRE(valid4_parsed == as_vector("11+33+*"));
+
+            // Statement with nested parenthesis
+            std::string valid5 = "((1+1)*3)";
+            std::vector<char> valid5_parsed;
+            REQUIRE_NOTHROW(valid5_parsed = parseStatement(valid5));
+            REQUIRE(valid5_parsed == as_vector("11+3*"));
+    }
+    SECTION("Invalid Statements"){
+        std::string invalid1 = "1+1-3*4/5^6+";
+        REQUIRE_THROWS(parseStatement(invalid1));
+
+    }
+
+}
+// This tests the program's ability to evaluate statements
+TEST_CASE("Non-RPN Evaluate"){
+    using namespace psv;
+    SECTION("Test basic addition") {
+        REQUIRE(nonRpnEvaluate("1 + 2") == 3.0f);
+    }
+
+    SECTION("Test basic subtraction") {
+        REQUIRE(nonRpnEvaluate("5 - 3") == 2.0f);
+    }
+
+    SECTION("Test basic multiplication") {
+        REQUIRE(nonRpnEvaluate("2 * 3") == 6.0f);
+    }
+
+    SECTION("Test basic division") {
+        REQUIRE(nonRpnEvaluate("10 / 2") == 5.0f);
+    }
+
+    SECTION("Test complex expression with parentheses") {
+        /* Let it be known that today was the day my own code went beyond
+         * ChatGPT...
+         * (Most all of these tests were generated by ChatGPT and/or GHCopilot)
+         * /.../math_matters/src/tests.cpp:301: Failure:
+                REQUIRE(nonRpnEvaluate("(1 + 3) * 2 + (1 - 32)") == -55.0f)
+                    with expansion:
+                -23.0f == -55.0f
+         * */
+//        REQUIRE(nonRpnEvaluate("(1 + 3) * 2 + (1 - 32)") == -55.0f);
+        // Correct answer is in fact -23.0f
+        REQUIRE(nonRpnEvaluate("(1 + 3) * 2 + (1 - 32)") == -23.0f);
+    }
+
+    SECTION("Test division by zero") {
+        REQUIRE_THROWS(nonRpnEvaluate("1 / 0"));
+//        REQUIRE_THROWS_AS(nonRpnEvaluate("1 / 0"), std::invalid_argument);
+    }
+
+    // TODO: Verify count of operators and operands
+    SECTION("Test invalid expression") {
+        REQUIRE_THROWS_AS(nonRpnEvaluate("1 + "), std::invalid_argument);
+    }
+}
+
