502ca9b15d7e86a1c98b70ba37bb0277e5cf5d26
Peter V <98245483+p5quared@users.noreply.github.com>
2023-04-03 09:30:28 -0400

    renders and works for the project scope

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,90 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <sstream>
 #include "input.hpp"
 
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/screen_interactive.hpp"
 
 int main() {
-    // more complicated statement
-    std::string eq2 = "1+1-(3*4)/5";
-    std::cout << "1+1-(3*4)/5= " << std::fixed << std::setprecision(2)
-    << psv::nonRpnEvaluate(eq2) << std::endl;
+    using namespace ftxui;
+    auto screen = ScreenInteractive::Fullscreen();
 
-    std::vector<char> parsed2 = psv::parseStatement(eq2);
-    // convert vector to string
-    std::string parsed2_str(parsed2.begin(), parsed2.end());
+    std::string statement;
+    float result;
+    std::string result_string;
+    std::stringstream result_stream;
+    bool evaluated = false;
+    Component input_statement = Input(&statement, "Enter a statement: ");
+
+    auto button_evaluate = Button("Evaluate", [&] {
+        try {
+            result_stream.str(std::string());
+            result = psv::nonRpnEvaluate(statement);
+            evaluated = true;
+            result_stream << std::fixed << std::setprecision(1) << result;
+            result_string = result_stream.str();
+        } catch (std::exception& e) {
+
+        }
+    });
+
+    auto document_mm = Container::Vertical({
+                                                input_statement,
+                                                button_evaluate,
+                                        });
+
+    auto Math_Matters = Renderer(document_mm, [&] {
+        return vbox({
+            filler(),
+            hbox({
+                input_statement->Render(),
+            }) | vcenter,
+            hbox({
+                text("Result: ") | bold,
+                text(result_string) | bold,
+            }) | vcenter,
+            filler(),
+            hbox({
+                button_evaluate->Render(),
+            }) | vcenter,
+            text("By Peter V.") | hcenter,
+        });
+    });
 
 
-    return 0;
+    auto Settings_Help = Renderer([&] {
+        return vbox({
+            text("Settings/Help") | hcenter,
+        });
+    });
+
+    int tab_index = 0;
+    std::vector<std::string> tabs = {"Statement Solver", "Settings/Help"};
+    auto tab_selection = Menu(&tabs, &tab_index, MenuOption::HorizontalAnimated());
+    auto tab_content = Container::Tab({
+        Math_Matters,
+        Settings_Help,
+    },
+    &tab_index);
+
+    auto document = Container::Vertical({
+        tab_selection,
+        tab_content,
+    });
+
+    auto main_renderer = Renderer(document, [&] {
+        return vbox({
+            text("Math-Matters") | bold | hcenter,
+            tab_selection->Render(),
+            tab_content->Render() | flex,
+        });
+    });
+
+
+    screen.Loop(main_renderer);
+
+    return EXIT_SUCCESS;
 }
